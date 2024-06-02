81bc7a5292035dc9fc68cc7de926eed2ba289aaa
Shubham Khade <shubhkdesign@gmail.com>
2024-06-02 18:00:36 +0530

    Completed adding reviews section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -132,18 +132,60 @@ export default function Home() {
                   <span className="bg-green-600">
                     print quality feels great
                   </span>
+                  This case feels amazing to the touch and is made of premium materials.s
                   "
                 </p>
+              
+              </div>
+              <div className="flex gap-2 mt-2">
+                  <img src="/users/user-1.png" className="rounded-full h-12 w-12 object cover" alt="testimonial image" />
+                  <div className="flex flex-col ">
+                    <p className="font-semibold">Shubham Khade</p>
+                    <div className="flex gap-1.5 items-center ">
+                      <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                      <p className="text-small">Verified purchase</p>
+                    </div>
+                  </div>
+              </div>
+            </div>
+          </div>
+          <div className="mx-auto grid max-w-2xl grid-cols-1 px-4 lg:mx-x-0 lg:max-w-none lg:grid-cols-2 gap-y-16">
+            <div className="flex flex-auto flex-col gap-4 lg:pr-8 xl:pr-20">
+              <div className="flex gap-0.5 mb-2">
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+                <Star className="h-5 w-5 text-green-600 fill-green-600" />
+              </div>
+              <div className="text-lg leading-8 ">
                 <p className="">
-                  The case feels durable and the print quality feels great
+                  "The case feels durable and the{" "}
+                  <span className="bg-green-600">
+                    print quality feels great
+                  </span>
+                  This case feels amazing to the touch and is made of premium materials.s
+                  "
                 </p>
-                <p className="">
-                  The case feels durable and the print quality feels great
-                </p>
+              
+              </div>
+              <div className="flex gap-2 mt-2">
+                  <img src="/users/user-1.png" className="rounded-full h-12 w-12 object cover" alt="testimonial image" />
+                  <div className="flex flex-col ">
+                    <p className="font-semibold">Shubham Khade</p>
+                    <div className="flex gap-1.5 items-center ">
+                      <Check className="h-4 w-4 stroke-[3px] text-green-600"/>
+                      <p className="text-small">Verified purchase</p>
+                    </div>
+                  </div>
               </div>
             </div>
           </div>
         </MaxWidthWrapper>
+
+        <div className="pt-16">
+          {/* <Reviews /> */}
+        </div>
       </section>
     </div>
   );
